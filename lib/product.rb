668722d49269139946b67b2f4fbac393ce6d5401
Grzegorz-Olszewski <grzegorz.olszewski10@gmail.com>
2016-02-12 17:49:19 +0100

    Added add_to_products method with eror DuplicateProductError.

--- lib/product.rb
+++ lib/product.rb
@@ -1,10 +1,23 @@
 class Product
+	attr_reader :title
 	@@products = []
 	def	initialize(options={})
-		@@products << self
+		@title = options[:title]
+		add_to_products
 	end
 
 	def self.all
 		@@products
+	end
+
+	private
+
+	def add_to_products
+		@@products.each do |toy| 
+			if toy.title == @title
+				raise DuplicateProductError, "That product already exists"
+			end
+		end
+		@@products << self
 	end
 end
