a447d1e7742208dae1be2e767553de0df597864a
Grzegorz-Olszewski <grzegorz.olszewski10@gmail.com>
2016-02-13 08:32:58 +0100

    attr_readers added, and also few methods in Product class.

--- lib/product.rb
+++ lib/product.rb
@@ -1,13 +1,42 @@
 class Product
-	attr_reader :title
+	attr_reader :title, :price, :stock
 	@@products = []
+
 	def	initialize(options={})
 		@title = options[:title]
+		@price = options[:price]
+		@stock = options[:stock]
 		add_to_products
 	end
 
 	def self.all
 		@@products
+	end
+
+	def self.find_by_title(title)
+		@title = title
+		@@products.each do |toy|
+			if toy.title == @title
+				return toy
+			end
+		end
+		raise NoSuchTitleError, "There is no toy with such title"
+	end
+	def in_stock?
+		if @stock == 0
+			return false
+		else 
+			return true
+		end
+	end
+	def self.in_stock
+		products_in_stock = []
+		@@products.each do |toy|
+			if toy.in_stock? == true
+				products_in_stock << toy
+			end
+		end
+		products_in_stock
 	end
 
 	private
